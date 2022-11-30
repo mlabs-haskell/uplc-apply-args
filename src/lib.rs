8d4a33d95feed39f6336324ca0bf9aa5a25f1537
zmrocze <karolochmanmilarski@gmail.com>
2022-11-30 20:33:19 +0100

    Apply argument takes list of arguments

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,14 @@
 mod utils;
 
+use uplc::{tx::error::Error, PlutusData};
 use wasm_bindgen::prelude::*;
 // use uplc::apply_params_to_script as uplc_apply_params_to_script;
+use uplc::{
+    ast::{DeBruijn, Program},
+    // machine::cost_model::ExBudget,
+    // PlutusData,
+};
+use pallas_primitives::{Fragment};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -15,12 +22,33 @@ pub fn strange(x : u32) -> u32 {
 }
 
 #[wasm_bindgen]
-pub fn apply_params_to_script(
+pub fn apply_params_array_to_script(
     params_bytes: &[u8], // PlutusData array
     plutus_script_bytes: &[u8],
 ) -> Result<Vec<u8>, String> {
     match uplc::tx::apply_params_to_script(params_bytes, plutus_script_bytes) {
         Ok(a) => Ok(a),
-        Err(e) => Err("error".to_string())
+        Err(_e) => Err("error".to_string())
+    }
+}
+
+
+pub fn apply_params_to_script(
+    params_bytes: Vec<&[u8]>, // PlutusData array
+    plutus_script_bytes: &[u8],
+) -> Result<Vec<u8>, Error> {
+
+    let mut buffer = Vec::new();
+    let mut program = Program::<DeBruijn>::from_cbor(plutus_script_bytes, &mut buffer)?;
+
+    for param in params_bytes {
+        let arg = PlutusData::decode_fragment(param).unwrap();
+
+        program = program.apply_data(arg);
+    }
+
+    match program.to_cbor() {
+        Ok(res) => Ok(res),
+        Err(_e) => Err(Error::ApplyParamsError),
     }
 }
