9ee4fa314435632aec27fb2cc1cf35e07dd6af0b
zmrocze <karolochmanmilarski@gmail.com>
2022-12-01 21:46:57 +0100

    0.1.5 apply_params_to_script_with_errors

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,8 @@
 mod utils;
 
-use uplc::{tx::error::Error, PlutusData};
 use wasm_bindgen::prelude::*;
-// use uplc::apply_params_to_script as uplc_apply_params_to_script;
+use uplc::{tx::error::Error, PlutusData};
+ // use uplc::apply_params_to_script as uplc_apply_params_to_script;
 use uplc::{
     ast::{DeBruijn, Program},
     // machine::cost_model::ExBudget,
@@ -12,43 +12,41 @@ use pallas_primitives::{Fragment};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
-#[cfg(feature = "wee_alloc")]
-#[global_allocator]
-static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+// #[cfg(feature = "wee_alloc")]
+// #[global_allocator]
+// static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+/// Apply parameters, provided as PlutusData list to a script. Returns string error.
+/// Err(e) translates to an exception on javascript side.
 #[wasm_bindgen]
-pub fn strange(x : u32) -> u32 {
-    return x + 1
-}
-
-#[wasm_bindgen]
-pub fn apply_params_array_to_script(
+pub fn apply_params_to_script(
     params_bytes: &[u8], // PlutusData array
     plutus_script_bytes: &[u8],
 ) -> Result<Vec<u8>, String> {
-    match uplc::tx::apply_params_to_script(params_bytes, plutus_script_bytes) {
-        Ok(a) => Ok(a),
-        Err(_e) => Err("error".to_string())
-    }
+    uplc::tx::apply_params_to_script(params_bytes, plutus_script_bytes).map_err(|e| e.to_string())
 }
 
-
-pub fn apply_params_to_script(
-    params_bytes: Vec<&[u8]>, // PlutusData array
+#[wasm_bindgen]
+pub fn apply_params_to_script_with_errors(
+    params_bytes: &[u8], // PlutusData array
     plutus_script_bytes: &[u8],
-) -> Result<Vec<u8>, Error> {
+) -> Result<Vec<u8>, String> {
+    let params = match PlutusData::decode_fragment(params_bytes) {
+        Ok(PlutusData::Array(res))  => Ok(res),
+        Ok(_) => Err("Couldn't decode as a plutus Array".to_string()),
+        Err(e) => Err(e.to_string())
+    }?;
 
     let mut buffer = Vec::new();
-    let mut program = Program::<DeBruijn>::from_cbor(plutus_script_bytes, &mut buffer)?;
+    let mut program = Program::<DeBruijn>::from_cbor(plutus_script_bytes, &mut buffer)
+        .map_err(|e| e.to_string())?;
 
-    for param in params_bytes {
-        let arg = PlutusData::decode_fragment(param).unwrap();
-
-        program = program.apply_data(arg);
+    for param in params {
+        program = program.apply_data(param);
     }
 
     match program.to_cbor() {
         Ok(res) => Ok(res),
-        Err(_e) => Err(Error::ApplyParamsError),
+        Err(_) => Err("Couldn't encode resulting script as CBOR.".to_string()),
     }
 }
