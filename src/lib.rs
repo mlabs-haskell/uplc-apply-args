62eb1da8ee8055beaba16a13dbf38f01d6359da9
zmrocze <karolochmanmilarski@gmail.com>
2022-12-13 16:10:57 +0100

    Remove debugs

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,5 @@
-mod utils;
-
 use wasm_bindgen::prelude::*;
-use uplc::{tx::error::Error, PlutusData};
- // use uplc::apply_params_to_script as uplc_apply_params_to_script;
-use uplc::{
-    ast::{DeBruijn, Program},
-    // machine::cost_model::ExBudget,
-    // PlutusData,
-};
+use uplc::{PlutusData, ast::{DeBruijn, Program}};
 use pallas_primitives::{Fragment};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
@@ -16,16 +8,11 @@ use pallas_primitives::{Fragment};
 // #[global_allocator]
 // static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
 
+
 /// Apply parameters, provided as PlutusData list to a script. Returns string error.
 /// Err(e) translates to an exception on javascript side.
-#[wasm_bindgen]
-pub fn apply_params_to_script(
-    params_bytes: &[u8], // PlutusData array
-    plutus_script_bytes: &[u8],
-) -> Result<Vec<u8>, String> {
-    uplc::tx::apply_params_to_script(params_bytes, plutus_script_bytes).map_err(|e| e.to_string())
-}
-
+/// 
+/// This is re-implementation of uplc::apply_params_to_script that doesn't panic on decoding errors.
 #[wasm_bindgen]
 pub fn apply_params_to_script_with_errors(
     params_bytes: &[u8], // PlutusData array
@@ -41,13 +28,9 @@ pub fn apply_params_to_script_with_errors(
     let mut program = Program::<DeBruijn>::from_cbor(plutus_script_bytes, &mut buffer)
         .map_err(|e| e.to_string())?;
     
-    println!("params: {:?}", params);
-    println!("program: {:?}", program);
-
     for param in params {
         program = program.apply_data(param);
     }
-    println!("applied program: {:?}", program);
     
     match program.to_cbor() {
         Ok(res) => Ok(res),
