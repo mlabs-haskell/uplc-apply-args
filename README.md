e7852da39ac2b8145b03c66c6be96b16674f3018
Vladimir Kalnitsky <vladimir@mlabs.city>
2024-07-19 15:32:58 +0400

    Update the uplc crate version

--- README.md
+++ README.md
@@ -9,8 +9,10 @@ This package defines a single "apply_params_to_script_no_panic" function.
 Here's a description on to how publish a newer version to the NPM registry:
 
 1. Install wasm-pack according to [wasm-pack docs](https://rustwasm.github.io/docs/wasm-pack/quickstart.html).
-2. Set the `package.name` in `Cargo.toml` to the correct name (i.e. `apply-args-browser`). This will become the npm package name.
-3. Bump the `package.version` in `Cargo.toml`.
+2. Set the `package.name` in `Cargo.toml` to the correct name (i.e. `apply-args-browser` or `apply-args-nodejs`. We can't use `@mlabs-haskell/` prefix here, because the name must be a valid crate name, but we can update it later in the generated package (step 5)).
+3. Bump the `package.version` in `Cargo.toml`. Set it to be equal to the version of the uplc crate.
 4. Run `wasm-pack build --target [bundler|nodejs]`.
-5. Run `wasm-pack publish --target [bundler|nodejs]`. You may need to login to the registry you want to publish to. You can login using `wasm-pack login`.
-6. Re-publish the NPM meta-package (located in `./npm-package`)
+5. Modify the generated npm package located in `./pkg`: Change its name to `@mlabs-haskell/`
+6. Publish the generated package.
+7. Repeat steps 1-6 for the `-nodejs` version too.
+8. Update the isomorphic (supporting both nodejs and the browser) package that is located in `./npm-package` and publish it
--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
 use wasm_bindgen::prelude::*;
-use uplc::{PlutusData, ast::{DeBruijn, Program}};
-use pallas_primitives::{Fragment};
+use uplc::{PlutusData, plutus_data, ast::{DeBruijn, Program}};
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -11,14 +10,14 @@ use pallas_primitives::{Fragment};
 
 /// Apply parameters, provided as PlutusData list to a script. Returns string error.
 /// Err(e) translates to an exception on javascript side.
-/// 
+///
 /// This is re-implementation of uplc::apply_params_to_script that doesn't panic on decoding errors.
 #[wasm_bindgen]
 pub fn apply_params_to_script_no_panic(
     params_bytes: &[u8], // PlutusData array
     plutus_script_bytes: &[u8],
 ) -> Result<Vec<u8>, String> {
-    let params = match PlutusData::decode_fragment(params_bytes) {
+    let params = match plutus_data(params_bytes) {
         Ok(PlutusData::Array(res))  => Ok(res),
         Ok(_) => Err("Couldn't decode as a plutus Array".to_string()),
         Err(e) => Err(e.to_string())
@@ -27,11 +26,11 @@ pub fn apply_params_to_script_no_panic(
     let mut buffer = Vec::new();
     let mut program = Program::<DeBruijn>::from_cbor(plutus_script_bytes, &mut buffer)
         .map_err(|e| e.to_string())?;
-    
+
     for param in params {
         program = program.apply_data(param);
     }
-    
+
     match program.to_cbor() {
         Ok(res) => Ok(res),
         Err(_) => Err("Couldn't encode resulting script as CBOR.".to_string()),
